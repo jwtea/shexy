82d68ccc360adb2efad2783ce67794bbb0c0305c
jwtea <jtaylor1205@hotmail.co.uk>
2018-02-06 14:56:53 +0000

    Render hex mesh and grid

---
+++ Assets/HexMetrics.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class HexMetrics {
+	public const float outerRadius = 10f;
+	public const float innerRadius = outerRadius * 0.866025404f;
+
+	//Define hex corners clockwise and oriented top bottom
+	public static Vector3[] corners = {
+		new Vector3(0f, 0f, outerRadius),
+		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(0f, 0f, -outerRadius),
+		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+		new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+		//todois this a hax ? stops overflow when final tri gets 
+		// seventh corner
+		new Vector3(0f, 0f, outerRadius)
+	};
+}
